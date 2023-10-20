5c27ce328f81c5f72fada329e8f64f126b07d25d
peang <pngo93@gmail.com>
2023-10-19 20:07:24 -0500

    frontend setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import configureStore from './store'
 import './index.css';
 import App from './App';
@@ -9,8 +10,12 @@ import App from './App';
 const store = configureStore()
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
+
 
 
 function Root() {
