b2ef44c756418c0e30aefdd4aa7b79eb214785f8
peang <pngo93@gmail.com>
2023-10-29 19:13:45 -0500

    additional seeders

--- backend/db/seeders/20230930235444-demo-reviews.js
+++ backend/db/seeders/20230930235444-demo-reviews.js
@@ -15,26 +15,26 @@ module.exports = {
     await Review.bulkCreate([
       {
         spotId: 1,
-        userId: 1,
+        userId: 2,
         review: 'If I could live here forever I would.',
         stars: 4
 
       },
       {
         spotId: 2,
-        userId: 2,
+        userId: 3,
         review: 'This was a wonderful place to stay, highly recommend.',
         stars: 4
       },
       {
         spotId: 3,
-        userId: 3,
+        userId: 4,
         review: 'I could stay here forever.',
         stars: 4
       },
       {
         spotId: 1,
-        userId: 2,
+        userId: 3,
         review: 'The stay was like a dream.',
         stars: 5
       },
@@ -43,6 +43,30 @@ module.exports = {
         userId: 2,
         review: 'Best vacation spot ever!',
         stars: 5
+      },
+      {
+        spotId: 5,
+        userId: 3,
+        review: '10/10 Highly recommend!',
+        stars: 5
+      },
+      {
+        spotId: 6,
+        userId: 4,
+        review: 'Would love to visit here again',
+        stars: 4
+      },
+      {
+        spotId: 7,
+        userId: 1,
+        review: 'Never been so relaxed',
+        stars: 5
+      },
+      {
+        spotId: 8,
+        userId: 1,
+        review: 'If only I had booked longer!',
+        stars: 4
       }
       
     ], { validate: true });
@@ -55,7 +79,7 @@ module.exports = {
     options.tableName = 'Reviews';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1,2,3,4] }
+      spotId: { [Op.in]: [1,2,3,4,5,6,7,8] }
     }, {});
 
   }
